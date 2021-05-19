a69c118deb3d427881668fb35c702e918b1add3f
Namratha157 <83855722+Namratha157@users.noreply.github.com>
2021-05-19 11:27:05 +0530

    Add files via upload

---
+++ Strings/src/string/Que4.java
@@ -0,0 +1,10 @@
+package string;
+
+public class Que4 {
+	public static void main(String[] args)
+	 {
+		Scanner sc=new Scanner(System.in);
+	    String str = sc.nextLine();    
+	    System.out.println(str.substring(0, str.length()/2));	
+	  } 
+}
---
+++ Strings/src/string/Que5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+package string;
+
+public class Que5 {
+	public static String
+    removeFirstandLast(String str)
+    {
+
+        str = str.substring(1, str.length() - 1);
+        return str;
+    }
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.print(
+            removeFirstandLast(str));
+    }
+}
